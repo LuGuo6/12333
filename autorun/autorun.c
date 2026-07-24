842ef6e26969ff0585636de9ba0adb888ec3c277
LuGuo6 <2561248563@qq.com>
2026-07-24 23:10:40 +0800

    新kpm

---
+++ autorun/autorun.c
@@ -0,0 +1,54 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+#include <compiler.h>
+#include <hook.h>
+#include <kpmodule.h>
+#include <kputils.h>
+#include <linux/kernel.h>
+#include <linux/printk.h>
+#include <linux/version.h>
+#include <linux/kmod.h>
+
+#include "autorun.h"
+
+#ifndef AUTORUN_VERSION
+#define AUTORUN_VERSION "1.0.0"
+#endif
+
+KPM_NAME("autorun");
+KPM_VERSION(AUTORUN_VERSION);
+KPM_LICENSE("GPL v2");
+KPM_AUTHOR("");
+KPM_DESCRIPTION("Auto-run script at /product/bin/Autorun");
+
+static long autorun_init(const char *args, const char *event, void *__user reserved)
+{
+    char *argv[] = { "/system/bin/sh", AUTORUN_SCRIPT_PATH, NULL };
+    char *envp[] = { "HOME=/", "PATH=/sbin:/system/sbin:/system/bin:/system/xbin", NULL };
+    int ret;
+
+    pr_info("autorun: executing %s\n", AUTORUN_SCRIPT_PATH);
+
+    ret = call_usermodehelper(argv[0], argv, envp, UMH_WAIT_PROC);
+    if (ret) {
+        pr_err("autorun: call_usermodehelper failed, ret=%d\n", ret);
+    } else {
+        pr_info("autorun: script executed successfully\n");
+    }
+
+    return 0;
+}
+
+static long autorun_exit(void *__user reserved)
+{
+    pr_info("autorun: exit\n");
+    return 0;
+}
+
+static long autorun_ctl0(const char *args, char *__user out_msg, int out_msg_len)
+{
+    return 0;
+}
+
+KPM_INIT(autorun_init);
+KPM_EXIT(autorun_exit);
+KPM_CTL0(autorun_ctl0);
---
+++ autorun/autorun.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+#ifndef _AUTORUN_H
+#define _AUTORUN_H
+
+#define AUTORUN_SCRIPT_PATH "/product/bin/Autorun"
+
+#endif /* _AUTORUN_H */
